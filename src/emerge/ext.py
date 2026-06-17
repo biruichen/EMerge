40a9254a86a7ee6af03a748eb6845ab7c47faa6d
Thomas-Zhou99 <233558248+Thomas-Zhou99@users.noreply.github.com>
2026-06-17 21:21:50 +0200

    added BC to emerge.ext

--- src/emerge/ext.py
+++ src/emerge/ext.py
@@ -1,4 +1,12 @@
 from ._emerge.solver import Solver, EigSolver, Preconditioner, Sorter
-from ._emerge.geometry import GeoVolume, GeoSurface, GeoEdge, GeoPoint, _FacePointer
+from ._emerge.geometry import (
+    GeoVolume,
+    GeoSurface,
+    GeoEdge,
+    GeoPoint,
+    _FacePointer,
+    GeoObject,
+    GeoPolygon,
+)
 from ._emerge.physics.microwave import bcs
 from ._emerge.physics.microwave.bcs.background_field import BackgroundField
